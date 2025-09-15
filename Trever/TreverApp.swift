a244dd068b0b3efdde903441e8cfe5457e518c9a
Sangyoon <qlrqod123123@naver.com>
2025-09-15 16:15:25 +0900

    Initial Commit

---
+++ Trever/TreverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreverApp.swift
+//  Trever
+//
+//  Created by 채상윤 on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TreverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
